e885d581eee9c8520c96066a04ee4bca9858b0e2
blp490 <blp490@BLP490.local>
2024-01-31 11:06:38 +0900

    restore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
 import Image from "next/image";
 import { getCanvas } from "./action";
+import { useEffect, useState } from "react";
 
-export default async function Home() {
- const imgSrc = await getCanvas();
+export default function Home() {
+ const [imgSrc, setImageSrc] = useState<string>();
+
+ useEffect(() => {
+  (async () => {
+   try {
+    const data = await getCanvas();
+    setImageSrc(data);
+   } catch (e) {
+    console.error(e);
+   }
+  })();
+ }, []);
  return imgSrc && <Image src={imgSrc} width={800} height={400} alt="" />;
 }
